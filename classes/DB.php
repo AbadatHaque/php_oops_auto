721ece2440c5a049f69ecbea745d688adaaa752f
Sk Abadat Haque <sksaddam7224@gmail.com>
2026-05-05 09:41:10 +0530

    connected with db

--- classes/DB.php
+++ classes/DB.php
@@ -2,11 +2,16 @@
 
 class DB{
     private static $_instance = null;
-    private $_pdo, $querry, $error=false, $result, $_count =0;
+    private $_pdo, $_query, $_error=false, $_result, $_count =0;
 
-    private function __constructor(){
+    private function __construct(){
         try {
-          $this->_pdo = new PDO('mysql:host='. Config::getValue('mysql/host'),'db_name='. Config::getValue('mysql/db'),Config::getValue('mysql/username'),Config::getValue('mysql/password'));
+        //   $this->_pdo = new PDO('mysql:host='. Config::getValue('mysql/host'),'db_name='. Config::getValue('mysql/db'),Config::getValue('mysql/username'),Config::getValue('mysql/password'));
+        $this->_pdo = new PDO(
+    'mysql:host=' . Config::getValue('mysql/host') . ';dbname=' . Config::getValue('mysql/db'),
+    Config::getValue('mysql/username'),
+    Config::getValue('mysql/password')
+);
         } catch (PDOException $e) {
             die($e->getMessage());
         }
@@ -17,6 +22,23 @@ class DB{
             self::$_instance = new DB();
         }
         return self::$_instance;
+    }
+
+    public function query($sql,$params=array()){
+        $this->_error = false;
+
+        if( $this->_query = $this->_pdo->prepare($sql) ){
+            if(count($params)){
+                $x=1;
+                foreach($params as $param){
+                    $this->_query->bindValue($x,$param);
+                    $x++;
+                }
+            }
+            if( $this->_query->execute() ) {
+                echo 'Successfully execute query';
+            }
+        }
     }
 
 }
--- index.php
+++ index.php
@@ -2,3 +2,5 @@
 require_once "core/init.php";
 
 echo Config::getValue('mysql/host');
+
+ DB::getInstance()->query("SELECT username from user WHERE username = ?", array('abadat'));
